4d46ac31ffac973ec676e0621df35e5d91d9eef3
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2023-01-10 23:00:09 -0500

    post-get

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
   }
-
+  getProducts(){
+    const response = this.httpClient.get('api.escuelajs.co/api/v1/products').subscribe
+    (response=> {console.log(response);
+    });
+  }
+  getProduct(){
+    const response = this.httpClient.get('api.escuelajs.co/api/v1/products/8').subscribe
+    (response=> {console.log(response);
+    });
+  }
+  createProduct(){
+    const data ={
+      title: 'Libros',
+      price: 15,
+      descripcion: 'Utiles escolares / Jair Delgado',
+      categoryId: 1,
+    }
+    const response = this.httpClient.post('api.escuelajs.co/api/v1/products/8',data).subscribe
+    (response=> {console.log(response);
+    });
+  }
 }
